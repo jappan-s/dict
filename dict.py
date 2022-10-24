09e5c4e52a3c6ccfb10a414dfb6ef5ce2aa0bb99
jappan-s <63873951+jappan-s@users.noreply.github.com>
2022-10-25 00:16:44 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+asciidict = dict()
+alfapetTeller = range(97,123)
+for i in alfapetTeller:
+    asciidict[str(i)] = chr(i)
+print(asciidict)
+
+
+# In[ ]:
+
+
+
+
